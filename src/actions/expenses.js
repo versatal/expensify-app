dc5203550ade69970698f2e58184d403d85e9f2d
Roger Hardin <volcivar@yahoo.com>
2018-02-07 06:56:15 -0500

    Set up remove expense from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -35,6 +35,15 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => { 
+      dispatch(removeExpense({ id }));
+    }).catch((e) => {console.log('Fetch failed.', e)});
+  };
+};
+
+
 //edit expense
 
 export const editExpense = (id, updates) => ({
